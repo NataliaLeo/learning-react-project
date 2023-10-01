5503802d95ca42feb8001904915cf59769c9370d
Наталья Л <housefuf@gmail.com>
2023-10-01 23:54:32 +0700

    initial commit

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutPage = () => {
+	return <div>MAIN PAGE</div>;
+};
+
+export default AboutPage;
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,25 @@
+import { FC, ReactElement, useMemo, useState } from "react";
+import { LOCAL_STORAGE_THEME_KEY, ThemeContext, Theme } from "./ThemeContext";
+
+const defaultTheme =
+	(localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
+
+const ThemeProvider = (props: { children: ReactElement }) => {
+	const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+	const defaultProps = useMemo(
+		() => ({
+			theme: theme,
+			setTheme: setTheme,
+		}),
+		[theme]
+	);
+
+	return (
+		<ThemeContext.Provider value={defaultProps}>
+			{props.children}
+		</ThemeContext.Provider>
+	);
+};
+
+export default ThemeProvider;
