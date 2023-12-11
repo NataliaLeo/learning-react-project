5fc2ab827670a66d6437dd14084f09d2e2613db3
Наталья Л <housefuf@gmail.com>
2023-12-12 02:03:05 +0700

    add Storybook, add Button stories

---
+++ config/storybook/preview.ts
@@ -0,0 +1,20 @@
+import type { Preview } from "@storybook/react";
+import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
+
+export const preview: Preview = {
+	parameters: {
+		actions: { argTypesRegex: "^on[A-Z].*" },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+};
+
+// export default preview;
+
+export default {
+	decorators: [StyleDecorator],
+};
