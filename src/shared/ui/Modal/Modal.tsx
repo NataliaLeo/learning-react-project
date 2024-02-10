53e776160b1bb29260340af30d882a7b436df47c
Наталья Л <housefuf@gmail.com>
2024-02-11 01:38:36 +0700

    fixed theme styles

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -10,10 +10,12 @@ interface ModalProps {
 	onClose?: () => void;
 }
 
+const ANIMATION_DELAY = 200;
+
 export const Modal = ({ className, children, isOpen, onClose }: ModalProps) => {
 	const [isClosing, setIsClosing] = useState(false);
 	const timeRef = useRef<ReturnType<typeof setTimeout>>();
-	const ANIMATION_DELAY = 200;
+
 	const closeHandler = useCallback(() => {
 		if (onClose) {
 			setIsClosing(true);
