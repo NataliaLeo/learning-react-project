fecae1dbb9370f2ea22bbe01bbe0bfe2c8e01c77
Наталья Л <housefuf@gmail.com>
2024-04-19 21:28:57 +0700

    Updates

---
+++ 297.54e36147ed365719a6cb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkulby_course=self.webpackChunkulby_course||[]).push([[297],{2297:(u,e,r)=>{r.r(e),r.d(e,{default:()=>t});var s=r(4848),c=r(2389);const t=function(){var u=(0,c.Bd)("about").t;return(0,s.jsx)("div",{children:u("O сайте")})}}}]);
